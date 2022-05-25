1d4991b1cf8b0ede98b3a0a9c58f94410f11bc34
carlosnicolas.patronelli.contractor <carlosnicolas.patronelli.contractor@gdevtools.com>
2022-05-25 17:11:22 -0300

    Elimino solucion-js del branch

--- solucion-js/app/dominio/personajes/Hulk.js
+++
@@ -1,9 +0,0 @@
-export class Hulk {
-    pelear() {
-        const random = Math.random();
-        if (random < 0.5)
-            console.log("Hulk se enfada y da un puñetazo con su brazo izquierdo ...");
-        else
-            console.log("Hulk se enfada y pelea utilizando su puño derecho ...");
-    }
-}
